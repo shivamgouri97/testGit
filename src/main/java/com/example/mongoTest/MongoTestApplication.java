c8cd6e2948ec469e471306fef58e033effd1023b
Shivam Gouri <shivamgouri@Shivams-MacBook-Pro.local>
2021-12-09 12:35:45 +0530

    first commit

---
+++ src/main/java/com/example/mongoTest/MongoTestApplication.java
@@ -0,0 +1,15 @@
+package com.example.mongoTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoTestApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Added First change");
+		SpringApplication.run(MongoTestApplication.class, args);
+
+	}
+
+}
